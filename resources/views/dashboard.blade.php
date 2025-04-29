5e8332dc36ed9f5460229bfcd9eed4dbc26e2ddd
Bibintanggg <bintangyudhaa8@gmail.com>
2025-04-29 14:03:10 +0700

    masih error

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
 
-    <div class="maxw-7xl mx-auto -py-7 px-4 sm:px-6 lg:px-8">
+    <div class="py">
         <div class="max-w-xl mx-auto px-8">
             <div class="bg-white overflow-hidden shadow-sm min-h-screen">
                 <div class="p-6 text-gray-900">
