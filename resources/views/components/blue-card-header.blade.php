cec456e34d88893b0810a1d7ef5f6b47bfc4f2e8
Bibintanggg <bintangyudhaa8@gmail.com>
2025-04-29 14:12:48 +0700

    error gain

---
+++ resources/views/components/blue-card-header.blade.php
@@ -0,0 +1,5 @@
+<div>
+    <div class="bg-blue-500 ">
+
+    </div>
+</div>
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,8 +3,11 @@
     <div class="py">
         <div class="max-w-xl mx-auto px-8">
             <div class="bg-white overflow-hidden shadow-sm min-h-screen">
+                <div style="background-color: blue; width: 50%; height: 50%;"></div>
                 <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+                    <h1 class="text-xl font-bold py-4 px-2">
+                        Hi, {{  Auth::user()->name }}!!
+                    </h1>
                 </div>
             </div>
         </div>
