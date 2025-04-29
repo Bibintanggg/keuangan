85c84b6abb216c4ce23de230a92b67f088299fcd
Bibintanggg <bintangyudhaa8@gmail.com>
2025-04-29 14:52:07 +0700

    fixing error

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,12 +1,12 @@
 <x-app-layout>
 
     <div class="py">
-        <div class="max-w-xl mx-auto px-8">
+        <div class="max-w-xl mx-auto -mt-10">
             <div class="bg-white overflow-hidden shadow-sm min-h-screen">
                 <div class="bg-black"></div>
                 <div class="p-6 text-gray-900">
-                    <h1 class="text-xl font-bold py-4 px-2">
-                        Hi, {{  Auth::user()->name }}!!
+                    <h1 class="text-xl font-bold mt-10 px-2">
+                        Hi, {{  Auth::user()->name }}!! !
                     </h1>
                 </div>
             </div>
