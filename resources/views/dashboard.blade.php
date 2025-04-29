ebc6b161638fbc4fa5e33289dc26036196389abf
Bibintanggg <bintangyudhaa8@gmail.com>
2025-04-29 13:46:23 +0700

    setup

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,8 +1,8 @@
 <x-app-layout>
 
-    <div class="py">
-        <div class="max-w-xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+    <div class="maxw-7xl mx-auto -py-7 px-4 sm:px-6 lg:px-8">
+        <div class="max-w-xl mx-auto px-8">
+            <div class="bg-white overflow-hidden shadow-sm min-h-screen">
                 <div class="p-6 text-gray-900">
                     {{ __("You're logged in!") }}
                 </div>
