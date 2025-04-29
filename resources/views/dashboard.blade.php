2b7a019214d3c90ec4ac854a26f8e12388756bad
Bibintanggg <bintangyudhaa8@gmail.com>
2025-04-29 14:22:38 +0700

    error

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,7 +3,7 @@
     <div class="py">
         <div class="max-w-xl mx-auto px-8">
             <div class="bg-white overflow-hidden shadow-sm min-h-screen">
-                <div style="background-color: blue; width: 50%; height: 50%;"></div>
+                <div class="bg-black"></div>
                 <div class="p-6 text-gray-900">
                     <h1 class="text-xl font-bold py-4 px-2">
                         Hi, {{  Auth::user()->name }}!!
