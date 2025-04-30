30719b87c0f4c5249790510828125e97ca4fb80e
Bibintanggg <bintangyudhaa8@gmail.com>
2025-04-30 08:08:08 +0700

    error contr

--- resources/views/components/dashboard-card.blade.php
+++ resources/views/components/dashboard-card.blade.php
@@ -2,4 +2,3 @@
     <h2 class="text-lg font-semibold">{{ $cardTitle }}</h2>
     <p class="text-2xl {{ $textColor }}">Rp {{ number_format($amount, 0, ',', '.') }}</p>
 </div>
-    
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,27 +5,27 @@
             <div class="bg-white overflow-hidden shadow-sm min-h-screen">
                 <div class="flex items-center justify-between p-6 text-gray-900">
                     <h1 class="text-xl font-bold mt-10 px-2">
-                        Hi, {{  Auth::user()->name }}!! !
+                        Hi, {{ Auth::user()->name }}!!
                     </h1>
                     <h2 id="ucapan-waktu" class="text-xl font-bold mt-10 px-2"></h2>
                 </div>
 
                 <div class="grid grid-cols-1 md:grid-cols-3 gap-4 p-6">
                     <x-dashboard-card
-                        title= "Total Pendapatan"
-                        :amount = "$totalIncome"
+                        title="Total Pendapatan"
+                        :amount= "$totalIncome"
                         bgColor="bg-green-500"
                         textColor="text-white"
                     />
                     <x-dashboard-card
-                        title= "Total Pengeluaran"
-                        :amount = "$totalExpense"
+                        title="Total Pengeluaran"
+                        :amount="$totalIncome"
                         bgColor="bg-red-500"
                         textColor="text-white"
                     />
                     <x-dashboard-card
-                        title= "Sisa Saldo"
-                        :amount = "$balance"
+                        title="Sisa Saldo"
+                        :amount="$balance"
                         bgColor="bg-blue-500"
                         textColor="text-white"
                     />
