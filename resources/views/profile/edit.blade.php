8d94c44736eaa44c18dba88c874754bc06a87a84
Bibintanggg <bintangyudhaa8@gmail.com>
2025-06-07 01:31:37 +0700

    esok lanjut

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,19 +1,19 @@
 <x-app-layout>
     <div class="py">
-        <div class="max-w-xl mx-auto px-8 space-y-6">
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+        <div class="max-w-[40rem] mx-auto px-8 -mt-3 -space-y-2">
+            <div class="p-4 sm:p-8 bg-white sm:rounded-lg">
                 <div class="max-w-xl">
                     @include('profile.partials.update-profile-information-form')
                 </div>
             </div>
 
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+            <div class="p-4 sm:p-8 bg-white  sm:rounded-lg">
                 <div class="max-w-xl">
                     @include('profile.partials.update-password-form')
                 </div>
             </div>
 
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+            <div class="p-4 sm:p-8 bg-white  sm:rounded-lg">
                 <div class="max-w-xl">
                     @include('profile.partials.delete-user-form')
                 </div>
